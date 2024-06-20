0cb62706e68eb9c0bffba437f5ad927ed8af6602
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-20 11:07:53 +0200

    add highest score reached to localStorage

--- src/custom_hooks.ts
+++ src/custom_hooks.ts
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react";
 
+type saveScore = {
+  score: number;
+  name: string;
+  time: string;
+};
+
 export function useCustomMessage(number: number, text: string) {
   const [message, setMessage] = useState<string | null>(null);
   const [t, setT] = useState<string[]>([]);
+
+  const score: saveScore = {
+    score: number,
+    name: "",
+    time: new Date().toLocaleString(),
+  };
+  let storage = [];
+
+  storage = JSON.parse(localStorage.getItem("scores")) || [];
 
   useEffect(() => {
     if ((number % 3) + (number % 5) == 0 && text == "") {
       setMessage(`Number ${number} is FizzBuzz`);
       setT([...t, `${number} = FizzBuzz`]);
+      storage.push(score);
     } else if (number % 3 == 0 && text == "") {
       setMessage(`Number ${number} is Fizz`);
       setT([...t, `${number} = Fizz`]);
+      storage.push(score);
     } else if (number % 5 == 0 && text == "") {
       setMessage(`Number ${number} is Buzz`);
       setT([...t, `${number} = Buzz`]);
+      storage.push(score);
     }
+
+    localStorage.setItem("scores", JSON.stringify(storage));
 
     console.log("NUmber: " + number + " " + text);
 

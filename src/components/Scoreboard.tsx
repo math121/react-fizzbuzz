9f2764e6c121685b761d37e18cdf02f68ddb2499
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-20 11:09:15 +0200

    implement scoreboard to display previous scores

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+
+type saveScore = {
+  score: number;
+  name: string;
+  time: string;
+};
+
 export const Scoreboard = () => {
-  return <h1>Scoreboard</h1>;
+  const [scores, setScores] = useState<saveScore[]>([]);
+
+  useEffect(() => {
+    const test = JSON.parse(localStorage.getItem("scores")) || [];
+    console.log(test);
+    setScores(test);
+  }, []);
+
+  return (
+    <>
+      <h1>Scoreboard</h1>
+      {scores &&
+        scores.map((value, index) => (
+          <p
+            key={index}
+          >{`Time: ${value.time} | Highest number reached: ${value.score}`}</p>
+        ))}
+
+      {scores.length == 0 && <p>No scores to show</p>}
+    </>
+  );
 };
